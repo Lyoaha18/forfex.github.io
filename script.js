7e1491a5c9bcc03bc5437bf00e8b36cf185dd57f
Lyoaha18 <alexey12098@gmail.com>
2024-06-20 19:35:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // JavaScript для модального окна
-const purchaseBtn = document.getElementById('purchaseBtn');
-const modal = document.getElementById('modal');
-const closeModal = document.querySelector('.close');
+document.addEventListener('DOMContentLoaded', function() {
+    const purchaseBtn = document.getElementById('purchaseBtn');
+    const modal = document.getElementById('modal');
+    const closeModal = document.querySelector('.close');
 
-purchaseBtn.addEventListener('click', function() {
-    modal.style.display = 'block';
-});
+    purchaseBtn.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
 
-closeModal.addEventListener('click', function() {
-    modal.style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    if (event.target == modal) {
+    closeModal.addEventListener('click', function() {
         modal.style.display = 'none';
-    }
-});
+    });
 
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
